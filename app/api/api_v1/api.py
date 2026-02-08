0c87a6497358495dd00ce4cb23feea101ce8592e
Suraja Hasarinda <surajahasarinda@gmail.com>
2026-02-08 23:41:28 +0530

    feat: add system stats monitoring

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import health, schedules, docker, auth
+from app.api.api_v1.endpoints import health, schedules, docker, auth, system
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(health.router, tags=["health"])
 api_router.include_router(schedules.router, prefix="/schedules", tags=["schedules"])
 api_router.include_router(docker.router, prefix="/docker", tags=["docker"])
+api_router.include_router(system.router, prefix="/system", tags=["system"])
